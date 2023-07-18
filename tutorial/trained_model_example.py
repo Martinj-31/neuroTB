c0a1b2dcd5ca7739c80835fbb6fa9a81503048b6
jjihk <jaeouk2001@gmail.com>
2023-07-18 17:39:58 +0900

    Create trained_model_example.py
    model을 train하지 않고 filepath만 이용하여 시뮬레이션을 진행할 때 사용하는 파일입니다.
    
    This file is used when simulation is performed using only the filepath without training the model.

---
+++ tutorial/trained_model_example.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 17:25:54 2023
+
+@author: user
+"""
+
+from neuroTB.main import main
+    
+main('C:/Users/user/Desktop/JSB/Repo/neuroTB/temp/07-17/171314')
