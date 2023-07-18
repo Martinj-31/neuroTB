a5c553b828c79724196081e14e362c744f2020fe
Mingyu Cheon <rjdls07@naver.com>
2023-07-18 16:16:41 +0900

    Initial commit for main.

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,4 +1,30 @@
-from ann_model_trainer import buildANNmodel
+import os
+os.chdir("./")
+print(os.getcwd())
+import configparser
+import neuroToolbox.neuPLUSNetwork as net
 
-model = buildANNmodel()
-model.summary()
+
+# %% Load data
+config = configparser.ConfigParser()
+
+
+
+# %% Parse model
+
+
+
+
+
+# %% Normalization and convert
+
+
+
+
+
+# %% Generate neurons and synapse connections for SNN
+batsh_shape = list(model.layers[0].input_shape)
+batsh_shape[0] = 1
+
+spike_model = net.networkGen(config, model)
+spike_model.setup_layers(batch_shape)
