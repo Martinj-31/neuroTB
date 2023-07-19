d17214d71e04f4275e69abed9f6bef3154ea2e88
jjihk <jaeouk2001@gmail.com>
2023-07-19 14:44:18 +0900

    Rename Directory
    main.py안에 main function이 정의되어 혼선을 피하기 위해 rename 하였습니다. (main -> run)
    
    rename the main function defined in main.py to avoid confusion (main -> run)

--- run/main.py
+++ run/main.py
@@ -1,19 +1,23 @@
-import os, sys
-sys.path.append(os.getcwd())
+import os
+import sys
 import configparser
+
+
+sys.path.append(os.getcwd())
+
 
 # import neuroToolbox.parse as parse
 # import neuroToolbox.normalization as norm
-import neuroToolbox.neuPLUSNetwork as net
+# import neuroToolbox.neuPLUSNetwork as net
 
-def main(config_filepath):
+def run_neuroTB(config_filepath):
     # %% Load data
     config = configparser.ConfigParser()
     config.read(config_filepath)
     
+    print("@@@@\n\n")
+    print("config : ", config)
     
-    print("config :", config)
-    print("@@@@")
     
     # %% Parse model
     
@@ -28,9 +32,11 @@ def main(config_filepath):
     
     
     # %% Generate neurons and synapse connections for SNN
+    '''
     batch_size = config["initial"]["batch_size"]
     batch_shape = list(model.layers[0].input_shape)
     batch_shape[0] = batch_size
     
     spike_model = net.networkGen(config, model)
     spike_model.setup_layers(batch_shape)
+    '''
