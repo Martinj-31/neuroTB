702649cec6dcae0591f6a4c8cb6f66695ebd5d3c
jjihk <jaeouk2001@gmail.com>
2023-07-21 15:07:03 +0900

    Update main.py
    - 상위 디렉토리의 경로를 추가
    - print_layer_connection

--- run/main.py
+++ run/main.py
@@ -5,8 +5,12 @@ from tensorflow.keras.models import load_model
 
 sys.path.append(os.getcwd())
 
+# Add the path of the parent directory (neuroTB) to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
 
-# import neuroToolbox.parse as parse
+import neuroToolbox.parse as parse
 # import neuroToolbox.normalization as norm
 # import neuroToolbox.neuPLUSNetwork as net
 
@@ -15,15 +19,19 @@ def run_neuroTB(config_filepath):
     config = configparser.ConfigParser()
     config.read(config_filepath)
     
-    # input_model_name에 저장된 모델 이름으로 저장된 모델을 불러와 해당 모델의 summary를 출력합니다.
-    input_model_name = config["paths"]["filename_ann"] # Read 'input_model' value from config.ini
-    input_model = load_model(os.path.join(config["paths"]["path_wd"], f"{input_model_name}.h5")) # Load the model using the input_model_name
+    # input_model_name에 저장된 모델 이름으로 저장된 모델을 불러와 해당 모델의 summary를 출력
+    
+    # Read 'input_model' value from config.ini
+    input_model_name = config["paths"]["filename_ann"]
+    # Load the model using the input_model_name
+    input_model = load_model(os.path.join(config["paths"]["path_wd"], f"{input_model_name}.h5")) 
     print("Summary of", input_model_name) # Print the summary of the loaded model
     input_model.summary()
     
     # %% Parse model
     
     parser = parse.Parser(input_model, config)
+    parser.print_layer_connections()
     parser.parse()
     
     
