fa20197a8df1bb2b8fa08ec68177de72b7a6fa7d
jjihk <jaeouk2001@gmail.com>
2023-08-11 10:35:51 +0900

    Update VGG16_CIFAR10_Keras2NeuralSim.py

--- tutorial/VGG16_CIFAR10_Keras2NeuralSim.py
+++ tutorial/VGG16_CIFAR10_Keras2NeuralSim.py
@@ -6,19 +6,62 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(parent_dir)
 
-
 import numpy as np
 import configparser
 from datetime import datetime
 from tensorflow import keras
 from run import main
+from neuroToolbox import parse
 
 
-path_wd = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(
-    __file__)), '..', 'temp', str(datetime.now().strftime("%m-%d" + "/" + "%H%M%S"))))
-os.makedirs(path_wd)
 
-print("path wd: ", path_wd)
+# Create a directory for saving outputs
+path_wd = os.path.join(parent_dir, 'temp', datetime.now().strftime("%m-%d/%H%M%S"))
+os.makedirs(path_wd, exist_ok=True)
+
+# Save the config file
+default_config_path = os.path.join("..", "default_config")
+
+# Load the default config file
+config = configparser.ConfigParser()
+config.read(default_config_path)
+
+# Update the config values with new values
+config['train settings'] = {
+    'loss': 'sparse_categorical_crossentropy',
+    'optimizer': 'adam',
+    'metrics': 'accuracy',
+    'validation_split': '0.1',
+    'callbacks': 'None',
+    'batch_size': '128',
+    'epochs': '1'
+}
+
+config['paths'] = {
+    'path_wd': path_wd,
+    'dataset_path': path_wd,
+    'x_train': os.path.join(path_wd, "x_train.npz"),
+    'x_test': os.path.join(path_wd, "x_test.npz"),
+    'y_train': os.path.join(path_wd, "y_train.npz"),
+    'y_test': os.path.join(path_wd, "y_test.npz"),
+    'filename_ann': 'cifar10_vgg16'
+}
+
+# Define path for the new config file
+config_filepath = os.path.join(path_wd, 'config')
+
+# Write the updated config values to a new file named 'config'
+with open(config_filepath, 'w') as configfile:
+    config.write(configfile)
+
+
+# Extract values from the config
+loss = config['train settings']['loss']
+optimizer_value = config['train settings']['optimizer']
+metrics = config['train settings']['metrics']
+validation_split = float(config['train settings']['validation_split'])
+batch_size = int(config['train settings']['batch_size'])
+epochs = int(config['train settings']['epochs'])
 
 # Load CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -27,21 +70,22 @@ print("path wd: ", path_wd)
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
-# One-hot encode the labels
-num_classes = 10
-y_train = y_train.reshape(-1)  # Convert one-hot encoded labels to categorical labels
-y_test = y_test.reshape(-1)  # Convert one-hot encoded labels to categorical labels
+# One-hot encode the labels if using 'categorical_crossentropy' loss
+if loss == 'categorical_crossentropy':
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
+    y_test = keras.utils.to_categorical(y_test, num_classes=10)
 
-# Save the dataset
+# Save the preprocessed dataset for later use
 np.savez_compressed(os.path.join(path_wd, 'x_train'), x_train)
 np.savez_compressed(os.path.join(path_wd, 'y_train'), y_train)
 np.savez_compressed(os.path.join(path_wd, 'x_test'), x_test)
 np.savez_compressed(os.path.join(path_wd, 'y_test'), y_test)
-# Extracting datasets for Normalization
 np.savez_compressed(os.path.join(path_wd, 'x_norm'), x_train[::10])
 
 # VGG16 model, with weights pre-trained on ImageNet.
 base_model = keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+pre_parser = parse.Parser(base_model, config)
+pre_parser.parse()
 
 # Add a new top layer
 x = base_model.output
@@ -49,26 +93,20 @@ x = keras.layers.GlobalAveragePooling2D()(x)  # This layer is optional
 x = keras.layers.Dense(4096, activation='relu')(x)
 x = keras.layers.Dropout(0.5)(x)
 x = keras.layers.Dense(4096, activation='relu')(x)
-
-outputs = keras.layers.Dense(num_classes, activation='softmax')(x)  # FC3
+outputs = keras.layers.Dense(10, activation='softmax')(x)  # FC3
 
 # This is the model we will train
 model = keras.Model(inputs=base_model.input, outputs=outputs)
 
 # First: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional layers to prevent large gradient updates wrecking the learned weights
 for layer in base_model.layers:
     layer.trainable = False
 
 # Compile the model
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=keras.optimizers.Adam(learning_rate=0.001),
-              metrics=['accuracy'])
+model.compile(loss=loss, optimizer=optimizer_value, metrics=[metrics])
 
 # Train the model
-batch_size = 128
-epochs = 1
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
 
 # Evaluate the model
 score = model.evaluate(x_test, y_test, verbose=0)
@@ -76,43 +114,8 @@ print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 # Save the model
-model_name = 'cifar10_vgg16'
-keras.models.save_model(model, os.path.join(path_wd, model_name + '.h5'))
+model_name = config['paths']['filename_ann']
+model_path = os.path.join(config['paths']['path_wd'], model_name + '.h5')
+keras.models.save_model(model, model_path)
 
-# Save the config file
-default_config_path = os.path.join("..", "default_config")
-
-# Load the default config file
-default_config = configparser.ConfigParser()
-default_config.read(default_config_path)
-
-# Update the config values with new values
-default_config['train settings'] = {
-    'loss': 'sparse_categorical_crossentropy',
-    'optimizer': 'adam',
-    'metrics': 'accuracy',
-    'validation_split': '0.1',
-    'callbacks': 'None' ,  # Callbacks can be added later if required
-    'batch_size': str(batch_size),
-    'epochs': str(epochs)
-}
-
-default_config['paths'] = {
-    'path_wd': path_wd,
-    'dataset_path': path_wd,
-    'x_train': os.path.join(path_wd, "x_train.npz"),
-    'x_test': os.path.join(path_wd, "x_test.npz"),
-    'y_train': os.path.join(path_wd, "y_train.npz"),
-    'y_test': os.path.join(path_wd, "y_test.npz"),
-    'filename_ann': model_name
-}
-
-
-# Define path for the new config file
-config_filepath = os.path.join(path_wd, 'config')
-
-# Write the updated config values to a new file named 'config'
-with open(config_filepath, 'w') as configfile:
-    default_config.write(configfile)
-
-main.run_neuroTB(config_filepath)  # Use run_neuroTB instead of run_n
+main.run_neuroTB(config_filepath)
