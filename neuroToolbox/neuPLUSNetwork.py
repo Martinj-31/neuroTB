d54f440d846c5620bb1c20e3ca7930eef47d63a5
Mingyu Cheon <rjdls07@naver.com>
2023-08-16 17:52:47 +0900

    Update convolutional layer mapping. Final.

--- neuroToolbox/neuPLUSNetwork.py
+++ neuroToolbox/neuPLUSNetwork.py
@@ -216,69 +216,78 @@ class networkGen:
 
         ii = 1 if keras.backend.image_data_format() == 'channels_first' else 0
 
-        input_channels = w.shape[2] # It is related to input feature map, which is the number of feature map.
-        output_channels = w.shape[3] # It is related to the number of filter.
-        height_fm = layer.input_shape[1 + ii] # Height of feature map.
-        width_fm = layer.input_shape[2 + ii] # Width of feature map.
-        height_kn, width_kn = layer.kernel_size # Width and height of kernel.
-        stride_y, stride_x = layer.strides # Strides.
+        input_channels = w.shape[2]
+        output_channels = w.shape[3]
+        height_fm = layer.input_shape[1 + ii]
+        width_fm = layer.input_shape[2 + ii]
+        height_kn, width_kn = layer.kernel_size
+        stride_y, stride_x = layer.strides
 
-        # To map neuron index, we need virtual feature map with CNN neuron index.
         fm = np.arange(width_fm*height_fm).reshape((width_fm, height_fm))
-
-        # numCols : Number of columns in output filters (horizontal moves).
-        # numRows : Number of rows in output filters (vertical moves).
         if 'valid' == layer.padding:
-            padding_y = 0 # Zero-padding rows.
-            padding_x = 0# Zero-padding columns.
-            numCols = (width_fm - width_kn + 1) // stride_x
-            numRows = (height_fm - height_kn + 1) // stride_y
+            padding_y = 0
+            padding_x = 0
+            numCols = int((width_fm - width_kn)/stride_x + 1)
+            numRows = int((height_fm - height_kn)/stride_y + 1)
             FM = fm
             for i in range(1, input_channels):
                 FM = np.concatenate((FM, fm+(width_fm*height_fm*i)), axis=0)
         elif 'same' == layer.padding:
-            padding_y = (height_kn - 1) // 2 # Zero-padding rows. It is (filter_size-1)/2.
-            padding_x = (width_kn - 1) // 2 # Zero-padding columns. It is (filter_size-1)/2.
-            numCols = width_fm // stride_x
-            numRows = height_fm // stride_y
-            # In 'same' padding type, surround feature map with -1.
+            padding_y = (height_kn - 1) // 2
+            padding_x = (width_kn - 1) // 2
+            numCols = int((width_fm - width_kn + 2*padding_x)/stride_x + 1)
+            numRows = int((height_fm - height_kn + 2*padding_y)/stride_y + 1)
             fm_pad = np.pad(fm, ((padding_y, padding_y), (padding_x, padding_x)), mode='constant', constant_values=-1)
             FM = fm_pad
             for i in range(1, input_channels):
                 FM = np.concatenate((FM, np.pad(fm+(width_fm*height_fm*i), ((padding_y, padding_y), (padding_x, padding_x)), mode='constant', constant_values=-1)), axis=0)
-        else:
-            raise NotImplementedError("Border_mode {} not supported".format(layer.padding))
-        
-        source = np.zeros(numCols*numRows*(height_kn*width_kn)*input_channels) # Index for layer before convolutional layer(feature map).
-        target = np.zeros(numCols*numRows*(height_kn*width_kn)*input_channels) # Index for layer after convolutional layer.
-        weights = np.zeros(numCols*numRows*(height_kn*width_kn)*input_channels)
+
+        height_fm = int(FM.shape[0]/input_channels)
+        width_fm = FM.shape[1]
+
+        FM = FM.flatten() # Make feature map flatten to indexing
+
+        source = np.zeros(numCols*numRows*(height_kn*width_kn)*input_channels*output_channels)
+        target = np.zeros(numCols*numRows*(height_kn*width_kn)*input_channels*output_channels)
+        weights = np.zeros(numCols*numRows*(height_kn*width_kn)*input_channels*output_channels)
 
         idx = 0
-        cnt = 1
-        row_idx = 0
-        fm_idx = 0
-        pad_idx = 0
-        for i in range(numCols*numRows*input_channels):
-            if 0 == i%numCols and i != 0:
-                row_idx += height_kn-1
-            for j in range(height_kn):
-                print(FM[fm_idx+row_idx+i+j*(width_fm+padding_y+padding_x):fm_idx+row_idx+i+j*(width_fm+padding_y+padding_x)+width_kn])
-                source[idx:idx+width_kn] = FM[fm_idx+row_idx+i+j*(width_fm+padding_y+padding_x):fm_idx+row_idx+i+j*(width_fm+padding_y+padding_x)+width_kn]
-                target[idx:idx+width_kn] = np.zeros(len(source[idx:idx+width_kn])) + i
-                # weights[idx:idx+width_kn] = w[j, 0:width_kn, 0, 0]
-                idx += width_kn
-            print('')
-            if 0 == cnt%(numCols*numRows):
-                fm_idx += ((width_fm+padding_y+padding_x)+(height_fm+padding_y+padding_x))
-                cnt = 1
-                continue
-            cnt += 1
-        # In 'same' padding type, remove padded index with -1.
+        fin = 0
+        target_cnt = 0
+        reset_cnt = 0
+        for fout in range(output_channels):
+            row_idx = 0
+            for i in range(numCols*numRows*input_channels):
+                if 0 == i%(numCols*numRows) and i != 0:
+                    if 'valid' == layer.padding:
+                        row_idx += 2*width_fm + width_kn - stride_x
+                    else:
+                        row_idx += 2*padding_y*width_fm + width_kn - stride_x
+                    fin += 1
+                    target_cnt = reset_cnt
+                    if fin == input_channels-1:    
+                        fin = 0
+                # When stride is more than 1,
+                elif 0 == i%numCols and i != 0:
+                    row_idx += width_fm*(stride_y-1) + width_kn - stride_x
+                for j in range(height_kn):
+                    source[idx:idx+width_kn] = FM[row_idx+i+(j*width_fm):row_idx+i+(j*width_fm)+width_kn]
+                    target[idx:idx+width_kn] = np.zeros(len(source[idx:idx+width_kn])) + target_cnt
+                    weights[idx:idx+width_kn] = w[j, 0:width_kn, fin, fout]
+                    idx += width_kn
+                row_idx += (stride_x-1)
+                target_cnt += 1
+            reset_cnt = target_cnt
+                
         if 'same' == layer.padding:
             padding_idx = np.where(source == -1)[0]
             source = np.delete(source, padding_idx)
             target = np.delete(target, padding_idx)
             weights = np.delete(weights, padding_idx)
+
+        source = source.astype(int)
+        target = target.astype(int)
+        weights = weights.astype(int)
 
         self.connections.append([source, target, weights])
 
