f387dab351565fa310a597929339d3e4da8f7bc1
jnsngbm <jaeouk2001@gmail.com>
2023-08-21 15:42:01 +0900

    change file name

---
+++ tutorial/CIFAR10_Keras2NeuralSim.py
@@ -0,0 +1,101 @@
+import os
+import sys
+
+# Add the path of the parent directory (neuroTB) to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
+import numpy as np
+import configparser
+from datetime import datetime
+from tensorflow import keras
+from run import main
+
+
+path_wd = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(
+    __file__)), '..', 'temp', str(datetime.now().strftime("%m-%d" + "/" + "%H%M%S"))))
+os.makedirs(path_wd)
+
+print("path wd: ", path_wd)
+
+# Load CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+# Data preprocessing and normalization
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# One-hot encode the labels
+num_classes = 10
+y_train = y_train.reshape(-1)  # Convert one-hot encoded labels to categorical labels
+y_test = y_test.reshape(-1)  # Convert one-hot encoded labels to categorical labels
+
+# Save the dataset
+np.savez_compressed(os.path.join(path_wd, 'x_test'), x_test)
+np.savez_compressed(os.path.join(path_wd, 'y_test'), y_test)
+# Extracting datasets for Normalization
+np.savez_compressed(os.path.join(path_wd, 'x_norm'), x_train[::10])
+
+
+# Define the input layer
+inputs = keras.Input(shape=(32, 32, 3))
+
+# Convolutional layers
+x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+x = keras.layers.BatchNormalization(epsilon=1e-5)(x)  # Add BatchNormalization layer
+x = keras.layers.Conv2D(32, (3, 3), activation='relu')(x)
+x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
+x = keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+x = keras.layers.BatchNormalization(epsilon=1e-5)(x)  # Add BatchNormalization layer
+x = keras.layers.Conv2D(64, (3, 3), activation='relu')(x)
+x = keras.layers.AveragePooling2D(pool_size=(2, 2))(x)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dense(6 * 6 * 64, activation='relu')(x)
+x = keras.layers.Dropout(0.25)(x)
+x = keras.layers.Dense(128, activation='relu')(x)
+
+outputs = keras.layers.Dense(num_classes, activation='softmax')(x)
+
+# Create the model
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+# Compile the model
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=keras.optimizers.Adam(learning_rate=0.001),
+              metrics=['accuracy'])
+
+# Train the model
+batch_size = 128
+epochs = 1
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+
+# Evaluate the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Save the model
+model_name = 'cifar10_cnn'
+keras.models.save_model(model, os.path.join(path_wd, model_name + '.h5'))
+
+# Save the config file
+default_config_path = os.path.join("..", "default_config")
+
+# Load the default config file
+default_config = configparser.ConfigParser()
+default_config.read(default_config_path)
+
+# Update the config values with new values
+default_config['paths']['path_wd'] = path_wd
+default_config['paths']['dataset_path'] = path_wd
+default_config['paths']['filename_ann'] = model_name
+
+# Define path for the new config file
+config_filepath = os.path.join(path_wd, 'config')
+
+# Write the updated config values to a new file named 'config'
+with open(config_filepath, 'w') as configfile:
+    default_config.write(configfile)
+
+main.run_neuroTB(config_filepath)  # Use run_neuroTB instead of run_n
