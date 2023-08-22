31e2d3f31a8fed8d28a75934d9221a0da3d0126f
Mingyu Cheon <rjdls07@naver.com>
2023-08-22 17:46:12 +0900

    Update.

--- neuroToolbox/evaluation.py
+++ neuroToolbox/evaluation.py
@@ -8,32 +8,40 @@ sys.path.append(parent_dir)
 import numpy as np
 import matplotlib.pyplot as plt
 
-from neuroToolbox.neuPLUSNetwork import networkGen as net
+import neuroToolbox.neuPLUSNetwork as net
 
 class evaluation:
-    def __init__(self, spike_model, config):
+    def __init__(self, parsed_model, config):
         self.config = config
-        self.model = spike_model
-        self.filepath = self.config.get('paths', 'path_wd')
-        self.filename = self.config.get('paths', 'filename_snn')
+        self.parsed_model = parsed_model
 
     def genLayer(self):
-        activates = np.load(self.filepath + '/activations/' + layer.name + '.npz')
+        print(f"")
+        print(f"SNN model will be stored layer by layer for evaluation.")
+        print(f"")
+        filepath = self.config.get('paths', 'evaluation_layers')
+        filename = self.config.get('paths', 'filename_snn')
+        os.makedirs(filepath)
 
-        for layer in self.model.layers[1:]:
+        temp = {}
+        eval = net.networkGen(self.parsed_model, self.config)
+        for layer in self.parsed_model.layers[1:]:
             layer_type = layer.__class__.__name__
-            print(f"Generate {layer_type} for evaluation.")
-            if layer_type == 'Flatten':
-                continue
-            elif layer_type == 'Dense':
-                net.Synapse_dense(layer, evaluation=True)
-                with open(self.filepath + '/' + self.filename + layer_type + '_eval.pkl'):
-                    pickle.dump()
-
+            if layer_type == 'Dense':
+                temp[layer_type] = eval.Synapse_dense(layer, evaluation=True)
+                with open(filepath + filename + '_' + layer.name + '_eval.pkl', 'wb') as f:
+                    pickle.dump(temp, f)
             elif 'Conv' in layer_type:
-                net.Synapse_convolution(layer, evaluation=True)
-                with open(self.filepath + '/' + self.filename + layer_type + '_eval.pkl'):
-                    pickle.dump()
+                temp[layer_type] = eval.Synapse_convolution(layer, evaluation=True)    
+                with open(filepath + filename + '_' + layer.name + '_eval.pkl', 'wb') as f:
+                    pickle.dump(temp, f)
+            else:
+                continue
+            print(f"Generate {layer_type} for evaluation.")
+        
+        print(f"")
+        print(f"All work is done.")
+        print(f"Take to your hardware.")
 
     def evaluate(self, layer):
         # Activates from DNN.
