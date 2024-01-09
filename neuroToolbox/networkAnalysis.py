c6ace5c079ea5c0bbd7fec6e6aa4962b2e874c19
Mingyu Cheon <rjdls07@naver.com>
2024-01-09 16:38:17 +0900

    Update networkAnalysis.py
    Add run method from networkCompiler.

--- neuroToolbox/networkAnalysis.py
+++ neuroToolbox/networkAnalysis.py
@@ -32,142 +32,95 @@ class Analysis:
             self.synapses = pickle.load(f)
         with open(self.snn_filepath + '_bias.pkl', 'rb') as f:
             self.bias_list = pickle.load(f)
-            
+    
 
-    def evalMapping(self, name='input'):        
-        if 'input' == name:
-            activation_dir = os.path.join(self.config['paths']['path_wd'], 'input_model_activations')
-            model = self.input_model
-        elif 'parsed' == name:
-            activation_dir = os.path.join(self.config['paths']['path_wd'], 'parsed_model_activations')
-            model = self.parsed_model
+    def run(self, image, label):
+        print(f"Preparing for running converted snn.")
+        x_test = image
+        y_test = label
+        syn_operation = 0
+
+        v_th = self.config.getint('conversion', 'threshold')
+        t_ref = self.config.getint('conversion', 'refractory') / 1000
+
+        print(f"Input data length : {len(x_test)}")
+        print(f"...\n")
+
+        print(f"Loading synaptic weights ...\n")
+        fr_dist = {}
+        w_dict = {}
+        w_cal = {}
+        for layer in self.synapses.keys():
+            fr_dist[layer] = []
+            w_dict[layer] = []
+            w_cal[layer] = []
         
-        synCnt = 0
-        w_list = []
-        for synapse in self.synapses.values():
-            src = np.array(synapse[0]) - synCnt
-            synCnt += 1024
-            tar = np.array(synapse[1]) - synCnt
-            w = np.array(synapse[2])
-            source = len(np.unique(src))
-            target = len(np.unique(tar))
-            weights = np.zeros(source * target).reshape(source, target)
-            
-            for i in range(len(w)):
-                weights[src[i]][tar[i]] = w[i]
-            w_list.append(weights)
-        
-        input_idx = 0
-        w_idx = 0
-        for layer in model.layers:
+        score = 0
+        for input_idx in range(len(x_test)):
+            synCnt = 0
+            firing_rate = x_test[input_idx].flatten()
+            for layer, neuron in self.synapses.items():
+                src = np.array(neuron[0]) - synCnt
+                synCnt += 1024
+                tar = np.array(neuron[1]) - synCnt
+                w = np.array(neuron[2])
+                source = len(np.unique(src))
+                target = len(np.unique(tar))
+                weights = np.zeros(source * target).reshape(source, target)
+                w_dict[layer] = w
+                for i in range(len(w)):
+                    weights[src[i]][tar[i]] = w[i]
 
-            print(f"Analysis for {layer.name} ...")
+                for neu_idx in range(len(firing_rate)):
+                    fan_out = len(np.where(weights[neu_idx][:] > 0)[0])
+                    syn_operation += firing_rate[neu_idx] * fan_out
 
-            if 'input' in layer.name:
-                input_idx += 1
-                continue
-            elif 'flatten' in layer.name:
-                input_idx += 1
-                continue
-            else:
-                input_act_file = np.load(os.path.join(activation_dir, f"input_model_activation_{layer.name}.npz"))
-                input_act = input_act_file['arr_0']
-            for neuron_idx, neuron in enumerate(self.neurons.keys()):
-                if 'input' in neuron:
-                    continue
+                firing_rate = np.dot(firing_rate, weights)
+                if 'conv' in layer:
+                    s = 0
+                    for oc_idx, oc in enumerate(neuron[3]):
+                        firing_rate[s:oc] = (firing_rate[s:oc] / v_th) + self.bias_list[layer][oc_idx]
+                        firing_rate[s:oc] = np.floor(firing_rate[s:oc] / (firing_rate[s:oc]*t_ref + v_th))
+                        s = oc
                 else:
-                    if np.prod(layer.output_shape[1:]) != list(self.neurons.values())[neuron_idx]:
-                        continue
-                    else:
-                        if 'batch' in layer.name:
-                            if neuron == model.layers[input_idx-1].name:
-                                neuron_name = model.layers[input_idx-2].name
-                            else: continue
-                        else:
-                            if layer.name == neuron:
-                                neuron_name = model.layers[input_idx-1].name
-                                if 'flatten' in neuron_name:
-                                    neuron_name = model.layers[input_idx-2].name
-                                else: pass
-                            else: continue
-                        loaded_activation_file = np.load(os.path.join(activation_dir, f"input_model_activation_{neuron_name}.npz"))
-                        loaded_activation = loaded_activation_file['arr_0']
-                        loaded_acts = []
-                        if 'input' in neuron_name:
-                            for ic in range(loaded_activation.shape[0]):
-                                temp = []
-                                for oc in range(loaded_activation.shape[-1]):
-                                    temp = np.concatenate((temp, loaded_activation[ic, :, :, oc].flatten()))
-                                loaded_acts.append(temp)
-                        elif 'conv' in neuron_name:
-                            for ic in range(loaded_activation.shape[0]):
-                                temp = []
-                                for oc in range(loaded_activation.shape[-1]):
-                                    temp = np.concatenate((temp, loaded_activation[ic, :, :, oc].flatten()))
-                                loaded_acts.append(temp)
-                        elif 'pooling' in neuron_name:
-                            for ic in range(loaded_activation.shape[0]):
-                                temp = []
-                                for oc in range(loaded_activation.shape[-1]):
-                                    for i in range(loaded_activation.shape[1]):
-                                        for j in range(loaded_activation.shape[2]):
-                                            temp = np.concatenate((temp, loaded_activation[ic, i, j, oc].flatten()))
-                                loaded_acts.append(temp)
-                        elif 'batch' in neuron_name:
-                            for ic in range(loaded_activation.shape[0]):
-                                temp = []
-                                for oc in range(loaded_activation.shape[-1]):
-                                    temp = np.concatenate((temp, loaded_activation[ic, :, :, oc].flatten()))
-                                loaded_acts.append(temp)
-                        elif 'dense' in neuron_name:
-                            for ic in range(loaded_activation.shape[0]):
-                                temp = []
-                                for oc in range(loaded_activation.shape[-1]):
-                                    temp = np.concatenate((temp, loaded_activation[ic, oc].flatten()))
-                                loaded_acts.append(temp)
-                        else: pass
-
-                        acts = []
-                        if 'conv' in neuron:
-                            for ic in range(input_act.shape[0]):
-                                for oc in range(input_act.shape[-1]):
-                                    acts = np.concatenate((acts, input_act[ic, :, :, oc].flatten()))
-                        elif 'pooling' in neuron:
-                            for ic in range(input_act.shape[0]):
-                                for oc in range(input_act.shape[-1]):
-                                    for i in range(input_act.shape[1]):
-                                        for j in range(input_act.shape[2]):
-                                            acts = np.concatenate((acts, input_act[ic, i, j, oc].flatten()))
-                        elif 'dense' in neuron:
-                            for ic in range(input_act.shape[0]):
-                                for oc in range(input_act.shape[-1]):
-                                    acts = np.concatenate((acts, input_act[ic, oc].flatten()))
-                        else: pass
-
-                        snn_fr = []
-                        if 'batch' in layer.name:
-                            w_idx -= 1
-                        else: pass
-                        for idx in range(len(loaded_acts)):
-                            firing_rate = loaded_acts[idx].flatten()
-                            firing_rate = np.dot(firing_rate, w_list[w_idx])
-                            neg_idx = np.where(firing_rate < 0)[0]
-                            firing_rate[neg_idx] = 0
-                            snn_fr = np.concatenate((snn_fr, firing_rate))
-                        
-                        plt.figure(figsize=(10, 10))
-                        plt.scatter(acts, snn_fr, color='b', marker='o', s=10)
-                        plt.title(f"DNN vs. SNN activation correlation", fontsize=30)
-                        plt.xlabel(f"Activations in {layer.name}", fontsize=27)
-                        plt.ylabel(f"Firing rate in {neuron}", fontsize=27)
-                        plt.xticks(fontsize=20)
-                        plt.yticks(fontsize=20)
-                        plt.grid(True)
-                        plt.savefig(self.config['paths']['path_wd'] + '/map_corr' + f"/{neuron}")
-                        plt.show()
-                    w_idx += 1
-            input_idx += 1
+                    firing_rate = firing_rate // v_th
+                    firing_rate = np.floor(firing_rate / (firing_rate*t_ref + v_th))
+                neg_idx = np.where(firing_rate < 0)[0]
+                firing_rate[neg_idx] = 0
+                fr_dist[layer] = np.concatenate((fr_dist[layer], firing_rate))
+            print(f"Firing rate from output layer for #{input_idx+1} input")
+            print(firing_rate)
             print('')
+
+            if np.argmax(y_test[input_idx]) == np.argmax(firing_rate):
+                score += 1
+            else: pass
+
+        for l in fr_dist.keys():
+            max_fr = np.max(fr_dist[l])
+            x_value = np.array([])
+            for f in range(int(max_fr)):
+                w_computed = f * w_dict[l]
+                neg_idx = np.where(w_computed < 0)[0]
+                w_computed[neg_idx] = 0
+                x_value = np.concatenate((np.ones(len(w_computed))*f, x_value))
+                w_cal[l] = np.concatenate((list(w_computed), w_cal[l])) 
+
+            plt.figure(figsize=(8, 6))
+            plt.plot(x_value, w_cal[l], 'b.')
+            plt.title(f"Firing rate * weight of {l} layer", fontsize=30)
+            plt.xlabel(f"Firing rate from 0 to maximum", fontsize=27)
+            plt.ylabel(f"Firing rates * weight", fontsize=27)
+            plt.yticks(fontsize=20)
+            plt.grid(True)
+            plt.savefig(self.config['paths']['path_wd'] + '/fr_distribution' + f"/{l}")
+            plt.show()
+
+        print(f"______________________________________")
+        print(f"Accuracy : {(score/len(x_test))*100} %")
+        print(f"Synaptic operation : {syn_operation}")
+        print(f"______________________________________\n")
+        print(f"End running\n\n")
             
 
     def evalNetwork(self):
