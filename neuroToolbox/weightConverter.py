ef3570ac2aeff28aa5e5333cfb1655f47d789bf2
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2024-03-12 11:15:58 +0900

    Update weightConverter.py

--- neuroToolbox/weightConverter.py
+++ neuroToolbox/weightConverter.py
@@ -2,6 +2,9 @@ import os, sys, pickle, math
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.integrate import quad
+from scipy.optimize import minimize_scalar
 
 import neuroToolbox.utils as utils
 
@@ -16,7 +19,7 @@ class Converter:
     Class for converting weight for SNN conversion.
     """
     
-    def __init__(self, parsed_model, config):
+    def __init__(self, spike_model, config):
         """
         Initialize the Converter instance.
 
@@ -24,23 +27,28 @@ class Converter:
             spike_model (tf.keras.Model): The compiled model for SNN from networkCompiler.
             config (configparser.ConfigParser): Configuration settings for weight conversion.
         """
+        self.neurons = spike_model[0]
+        self.synapses = spike_model[1]
         self.config = config
         self.input_model_name = config["names"]["input_model"]
+        
+        bias_flag = config["model"]["bias"]
+        if bias_flag == 'False':
+            self.bias_flag = False
+        elif bias_flag == 'True':
+            self.bias_flag = True
+        else: print(f"ERROR !!")
 
-        self.parsed_model = parsed_model
-        # self.parsed_model = tf.keras.models.load_model(os.path.join(self.config["paths"]["models"], f"parsed_{self.input_model_name}.h5"))
+        self.parsed_model = tf.keras.models.load_model(os.path.join(self.config["paths"]["models"], f"parsed_{self.input_model_name}.h5"))
         
         if 'LIF' == config["conversion"]["neuron"]:
-            self.v_th = config.getint('LIF', 'threshold')
+            self.v_th = config.getfloat('LIF', 'threshold')
             self.t_ref = config.getint('LIF', 'refractory') / 1000
-            alpha = config.getint('LIF', 'alpha')
+            self.w_mag = config.getfloat('LIF', 'w_mag')
+            max_ratio = config.getfloat('LIF', 'max_ratio')
+            # self.alpha = (self.v_th*max_ratio) / ((1-max_ratio)*self.t_ref)
         elif 'IF' == config["conversion"]["neuron"]:
             self.v_th = config.getint('IF', 'threshold')
-
-        # self.min = 1/self.t_ref * lower_bound
-        # self.max = 1/self.t_ref * upper_bound
-        # self.min_bound = self.min / (self.v_th - self.min*self.t_ref)
-        # self.max_bound = self.max / (self.v_th - self.max*self.t_ref)
 
         self.filepath = self.config['paths']['models']
         self.filename = self.config['names']['snn_model']
@@ -55,37 +63,76 @@ class Converter:
 
         print("\n\n######## Converting weight ########\n")
         
-        # weights = utils.weightDecompile(self.synapses)
+        weights = utils.weightDecompile(self.synapses)
         
         if 'LIF' == self.config["conversion"]["neuron"]:
             
             print(f">>Conversion for LIF neuron.\n")
             
+            # def log_lif(x, beta, T, V):
+            #     if x == 0:
+            #         return 0
+            #     return (1/V)*x**(np.log(beta/(beta*T + V)) / np.log(beta))
+            
+            # def lif(x, T, V):
+            #     return x/(x*T + V)
+            
+            # def difference_of_integrals(beta):
+            #     integral_A, _ = quad(lambda x: lif(x, self.t_ref, self.v_th) - log_lif(x, beta, self.t_ref, self.v_th), 0, beta)
+            #     integral_B, _ = quad(lambda x: log_lif(x, beta, self.t_ref, self.v_th) - lif(x, self.t_ref, self.v_th), beta, self.alpha)
+            #     return abs(integral_B - integral_A)
+            
+            # v_th = {}
             for layer in self.parsed_model.layers:
                 
-                if 'input' in layer.name or 'flatten' in layer.name:
+                if 'input' in layer.name:
+                    firing_rate = x_norm
+                    continue
+                elif 'flatten' in layer.name:
                     continue
                 else: pass
                 
-                print(f" Weight conversion for {layer.name} layer...")
+                print(f" Weight conversion for {layer.name} layer...\n")
+
+                neuron = self.synapses[layer.name]
                 
-                weights = layer.get_weights()
-            
+                if self.bias_flag:
+                    if 'conv' in layer.name or 'dense' == layer.name:
+                        ann_weights = [weights[layer.name], neuron[3]]
+                        print(ann_weights[1])
+                    else: ann_weights = [weights[layer.name]]
+                else: ann_weights = [weights[layer.name]]
+                
+                # min_value = minimize_scalar(difference_of_integrals, bounds=(0, self.alpha), method='bounded')
+                # beta = min_value.x
+                
+                # Weight normalization
+                max_ann_weights = np.max(abs(ann_weights[0]))
+                snn_weights = ann_weights[0] / max_ann_weights * self.w_mag
+                # snn_weights = ann_weights[0]
+                
+                if self.bias_flag:
+                    if 'conv' in layer.name or 'dense' == layer.name:
+                        neuron[2] = snn_weights
+                        neuron[3] = ann_weights[1]
+                    else: neuron[2] = snn_weights
+                else: neuron[2] = snn_weights
+                
+            with open(self.filepath + self.filename + '_Converted_synapses.pkl', 'wb') as f:
+                pickle.dump(self.synapses, f)
+                
+        # Need to edit
         elif 'IF' == self.config["conversion"]["neuron"]:
             
             print(f">>Conversion for IF neuron.\n")
             
             batch_size = self.config.getint('conversion', 'batch_size')
-            
             norm_facs = {self.parsed_model.layers[0].name: 1.0}
-            max_weight_values = {}
             
-            i = 0
             for layer in self.parsed_model.layers:
                 
-                if 'input' in layer.name or 'flatten' in layer.name:
+                if len(layer.weights) == 0:
                     continue
-                else: pass
                 
                 activations = self.get_activations_layer(self.parsed_model, layer, x_norm, batch_size, activation_dir)
                 
@@ -98,19 +145,13 @@ class Converter:
                 norm_facs[layer.name] = cliped_max_activation
                 print(f"Cliped maximum activation: {norm_facs[layer.name]:.5f}.\n")
                 i += 1
-                
-            for layer in self.parsed_model.layers:
-                print(layer.name)
-                if 'input' in layer.name or 'flatten' in layer.name:
-                    continue
-                else: pass
 
-                parameters = layer.get_weights()
-                if 'pooling' in layer.name:
-                    ann_weights = parameters
-                else:
-                    ann_weights = parameters[0]
-                    bias = parameters[1]
+            for layer in self.parsed_model.layers:
+
+                if len(layer.weights) == 0:
+                    continue
+
+                ann_weights, bias = layer.get_weights()
                     
                 if layer.activation.__name__ == 'softmax':
                     norm_fac = 1.0
@@ -118,21 +159,15 @@ class Converter:
                 else:
                     norm_fac = norm_facs[layer.name]
                     
-                inbound = utils.get_inbound_layers_with_params(layer)
+                inbound = self.get_inbound_layers_with_params(layer)
                 
                 if len(inbound) == 0:
-                    if 'pooling' in layer.name:
-                        snn_weights = [ann_weights * norm_facs[self.parsed_model.layers[0].name] / norm_fac]
-                    else:
-                        snn_weights = [ann_weights * norm_facs[self.parsed_model.layers[0].name] / norm_fac, bias / norm_fac]
-                    print("\n +++++ input norm_facs +++++ \n ", norm_facs[self.model.layers[0].name])
+                    snn_weights = [ann_weights * norm_facs[self.parsed_model.layers[0].name] / norm_fac, bias / norm_fac]
+                    print("\n +++++ input norm_facs +++++ \n ", norm_facs[self.parsed_model.layers[0].name])
                     print("  ---------------")
                     print(" ", norm_fac)
                 elif len(inbound) == 1:
-                    if 'pooling' in layer.name:
-                        snn_weights = [ann_weights * norm_facs[inbound[0].name] / norm_fac]
-                    else:
-                        snn_weights = [ann_weights * norm_facs[inbound[0].name] / norm_fac, bias / norm_fac]
+                    snn_weights = [ann_weights * norm_facs[inbound[0].name] / norm_fac, bias / norm_fac]
                     print("\n +++++ norm_facs +++++\n ", norm_facs[inbound[0].name])
                     print("  ---------------")
                     print(" ", norm_fac)
@@ -143,73 +178,25 @@ class Converter:
         else: pass
         
         print(f"\nWeight conversion DONE.<<<\n\n\n")
-
-        for layer in self.parsed_model.layers:
-
-            if 'input' in layer.name or 'flatten' in layer.name:
-                continue
-
-            print(f" Weight conversion for {layer.name} layer...")
-
-            neuron = self.synapses[layer.name]
-
-            w = np.array(neuron[2])
-            if 'conv' in layer.name or layer.name == 'dense':
-                bias = neuron[3]
-            else: pass
-            
-            if 'pooling' in layer.name:
-                neuron[2] = weights[layer.name]
-                continue
-            else: pass
-
-            inbound = utils.get_inbound_layers_with_params(layer)
-            pre_layer = inbound[0]
-            pre_activation_file = np.load(os.path.join(activation_dir, f"parsed_model_activation_{pre_layer.name}.npz"))
-            cur_activation_file = np.load(os.path.join(activation_dir, f"parsed_model_activation_{layer.name}.npz"))
-            
-            pre_activation = pre_activation_file['arr_0']
-            cur_activation = cur_activation_file['arr_0']
-
-            # median = np.median(pre_activation)
-            # scale = self.max_bound / median
-            # weights[layer.name] = weights[layer.name] * scale
-            
-            # weight calculation
-
-            new_bias = bias
-            neuron[2] = weights[layer.name]
-            if 'conv' in layer.name or layer.name == 'dense':
-                neuron[3] = new_bias
-            else: pass
-
-        with open(self.filepath + self.filename + '_Converted_synapses.pkl', 'wb') as f:
-            pickle.dump(self.synapses, f)
-
-        print(f"\nWeight conversion DONE.<<<\n\n\n")
     
 
-    def get_spikes(self, model, layer_in, layer_out, x):
-        self.remove_keys(model, layer_in)
-        weights = utils.weightDecompile(self.synapses)
-
+    def get_spikes(self, x, weights, layer_name):
+        neuron = self.synapses[layer_name]
+        input_spikes = x
         spikes = []
-        for input_idx in range(len(x)):
-            firing_rate = x[input_idx].flatten()
-            for layer, neuron in model.items():
-                firing_rate = np.dot(firing_rate, weights[layer])
-                if 'conv' in layer:
-                    s = 0
-                    for oc_idx, oc in enumerate(neuron[4]):
-                        firing_rate[s:oc] = firing_rate[s:oc] + neuron[3][oc_idx]
-                        s = oc + 1
-                elif 'dense' in layer:
-                    firing_rate = firing_rate + neuron[3]
-                else: pass
-                neg_idx = np.where(firing_rate < 0)[0]
-                firing_rate[neg_idx] = 0
-                if layer == layer_out:
-                    break
+        for input_idx in range(len(input_spikes)):
+            firing_rate = input_spikes[input_idx].flatten()
+            firing_rate = np.dot(firing_rate, weights)
+            if 'conv' in layer_name:
+                s = 0
+                for oc_idx, oc in enumerate(neuron[4]):
+                    firing_rate[s:oc] = firing_rate[s:oc] + neuron[3][oc_idx]
+                    x = oc + 1
+            elif 'dense' in layer_name:
+                firing_rate = firing_rate + neuron[3]
+            else: pass
+            neg_idx = np.where(firing_rate < 0)[0]
+            firing_rate[neg_idx] = 0
             spikes.append(firing_rate)
         
         return np.array(spikes)
@@ -268,3 +255,59 @@ class Converter:
     def get_percentile_activation(self, activations, percentile):
 
         return np.percentile(activations, percentile) if activations.size else 1
+    
+    
+    def get_inbound_layers_with_params(self, layer):
+        
+        inbound = layer
+        prev_layer = None
+        # Repeat when a layer with weight exists
+        while True:
+            inbound = self.get_inbound_layers(inbound)
+            
+            if len(inbound) == 1 and not isinstance(inbound[0], 
+                                                    tf.keras.layers.BatchNormalization):
+                inbound = inbound[0]
+                if self.has_weights(inbound):
+                    return [inbound]
+                
+            # If there is no layer information
+            # In the case of input layer, the previous layer does not exist, 
+            # so it is empty list return
+            else:
+                result = []
+                for inb in inbound:
+                    if isinstance(inb, tf.keras.layers.BatchNormalization):
+                        prev_layer = self.get_inbound_layers_with_params(inb)[0]
+                            
+                    if self.has_weights(inb):
+                        result.append(inb)
+                        
+                    else:
+                        result += self.get_inbound_layers_with_params(inb)
+                        
+                if prev_layer is not None:
+                    return [prev_layer]
+                
+                else:
+                    return result
+    
+    
+    def get_inbound_layers(self, layer):
+        
+        # Check the previous layer of that layer through _inbound_nodes
+        inbound_layers = layer._inbound_nodes[0].inbound_layers
+        
+        if not isinstance(inbound_layers, (list, tuple)):
+            inbound_layers = [inbound_layers]
+            
+        return inbound_layers
+        
+    
+    def has_weights(self, layer):
+        
+        if isinstance(layer, tf.keras.layers.BatchNormalization):
+            return False
+        
+        else:    
+            return len(layer.weights)
