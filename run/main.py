cc6881ce0a178cac33800b5e9302a393859f3ba9
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2024-04-08 12:11:17 +0900

    Update main.py

--- run/main.py
+++ run/main.py
@@ -76,14 +76,14 @@ def run_neuroTB(config_filepath):
     
     converter.convertWeights()
     threshold = converter.get_threshold()
-    error_list = converter.error()
+    error_list, synops_error_list, acc_error_list, firing_range_list = converter.error()
     
     
     # %% Evaluation each step
     evaluation = networkAnalysis.Analysis(config)
     
     # evaluation.input_log_domain_trans_plot()
-    evaluation.plot_error(error_list)
+    evaluation.plot_error(error_list, synops_error_list, acc_error_list)
     evaluation.set_threshold(threshold)
     evaluation.run(data_size)
     # evaluation.IOcurve()
